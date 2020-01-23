126dfeed05c34c59f814cf07d0e4dbfb3fed0c56
Cahillmn <mcahill@seattleu.edu>
2020-01-23 08:35:09 -0800

    finished 1-fizzbuzz

--- 1-fizzbuzz/index.js
+++ 1-fizzbuzz/index.js
@@ -1,8 +1,8 @@
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: a program that takes a string as input and counts how many of each character are in that string.
+Input: an Integer
+Output: a list of numbers, with fizz if divisible by 3, buzz if divisible by 5, and fizzbuzz if sivisible by both.
+Usage: node 1-fizzbuzz <integer>
 */
 
 // get input using getargs
@@ -11,20 +11,19 @@ let input = getargs.getIntegerArg()
 
 // test that input is a number and exit with error
 // TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage: node 1-fizzbuzz <integer>");
 }
 else {
   // iterate from 1 to the value of input
-  // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
+  for (let i = 1; i<=input; i++ ) {
     
     //TODO provide for tests for divisible for 3,5, and 15 to create desired output
-    if ( ) {
+    if (i % 15 == 0) {
       console.log(`${i} fizzbuzz`);
-    } else if ( ) {
+    } else if (i % 3 == 0) {
       console.log(`${i} fizz`);
-    } else if ( ) {
+    } else if (i % 5 ==0) {
       console.log(`${i} buzz`);
     } else {
       console.log(`${i}`);
