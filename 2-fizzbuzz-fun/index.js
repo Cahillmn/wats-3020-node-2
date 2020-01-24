8382bde5d9f86c7a14946cc3cb2343e50c3661cf
Cahillmn <mcahill@seattleu.edu>
2020-01-23 18:16:13 -0800

    finished 2-fizzbuzz-fun

--- 2-fizzbuzz-fun/index.js
+++ 2-fizzbuzz-fun/index.js
@@ -1,8 +1,9 @@
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: an adaptation of the FizzBuzz algorithm but with a function.
+Input: an Integer
+Output: a list of numbers, with fizz if divisible by 3, buzz if divisible by 5, and fizzbuzz if sivisible by both.
+Usage: node 1-fizzbuzz-fun <integer>
+
 */
 
 // get input using getargs
@@ -10,19 +11,25 @@ const getargs = require("../modules/getargs")
 let input = getargs.getIntegerArg()
 
 
-function fizzbuzz(n){
-// TODO provide for tests for divisible for 3,5, and 15 to return desired output
-}
-
-// TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
-}
-else {
-  // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
-    console.log(`${i} ${fizzbuzz(i)}`);
+function fizzbuzz(n) {
+  if (n % 15 == 0) {
+    return 'fizzbuzz';
+  } else if (n % 3 == 0) {
+    return `fizz`;
+  } else if (n % 5 == 0) {
+    return `buzz`;
+  } else {
+    return ``;
   }
 }
 
 
+
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage: node 1-fizzbuzz <integer>");
+} else {
+
+  for (let i = 1; i <= input; i++) {
+    console.log(`${i} ${fizzbuzz(i)}`);
+  }
+}
